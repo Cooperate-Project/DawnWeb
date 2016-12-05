5f854b05f4501ec0f9617eb3bfe09a71eaa6552d
shengjia <shengjia@DESKTOP-D5TO4B3.local>
2016-12-05 13:02:10 +0100

    Added shortcut to inspect whether an element is editable or removable

--- bundles/org.eclipse.emf.cdo.dawn.web.js.draw2d/web_content/javascript/treeviewJs.js
+++ bundles/org.eclipse.emf.cdo.dawn.web.js.draw2d/web_content/javascript/treeviewJs.js
@@ -43,7 +43,8 @@ function treeview(treeID) {
 		lettere:	69,
 		letterd: 	68,
 		lettera:  65,
-		lettert:  84
+		lettert:  84,
+		letterp:  80
 	};
 
 	// initialize the treeview
@@ -395,23 +396,43 @@ treeview.prototype.handleKeyDown = function($item, e) {
 
 		}
 
+		case this.keys.letterp: {
+			// Displays the properties of the current object in the status bar
+			if ($item.hasClass('mutable')) {
+				if ($item.hasClass('removable')) {
+					changeStatus('You can edit or remove this element.');
+				} else {
+					changeStatus('You can edit this element, but you can not remove it.');
+				}
+			} else {
+				if ($item.hasClass('removable')) {
+					changeStatus('You can remove, but not edit this element.');
+				} else {
+					changeStatus('This element cannot be edited or deleted.');
+				}
+			}
+
+			e.stopPropagation();
+			return false;
+		}
+
 		case this.keys.lettert: {
 
 			// Shortcut for toggle between the hierarchy types
-	    // Toggle graphical visibility of the lists
-	    toggleVisibility($('#SyntaxHierarchy'));
+			// Toggle graphical visibility of the lists
+			toggleVisibility($('#SyntaxHierarchy'));
 			toggleVisibility($('#ClusterHierarchies'));
 
-	    // If there was an element focused, focus the corresponding element again
-	    if (typeof $item.attr('id') != 'undefined') {
-	      getCounterPart($item).focus();
-	    }
+			// If there was an element focused, focus the corresponding element again
+			if (typeof $item.attr('id') != 'undefined') {
+				getCounterPart($item).focus();
+			}
 
-	    changeStatus('Changed perspective. You are now viewing the '
-	    + ($('#SyntaxHierarchy').is(':visible') ? 'syntax hierarchy' : 'cluster hierarchies') + '.');
+			changeStatus('Changed perspective. You are now viewing the '
+			+ ($('#SyntaxHierarchy').is(':visible') ? 'syntax hierarchy' : 'cluster hierarchies') + '.');
 
-	    e.stopPropagation();
-	    return false;
+			e.stopPropagation();
+			return false;
 
 		}
 
@@ -468,7 +489,7 @@ treeview.prototype.handleKeyDown = function($item, e) {
 					$('#' + $item.data('referenced-element-id')).focus();
 					changeStatus('Jumped to reference.');
 				} else {
-					changeStatus("Failed to jump to reference, the element might be collapsed.");
+					changeStatus("Failed to jump to reference, the element might be hidden right now.");
 				}
 			}
 
@@ -485,14 +506,14 @@ treeview.prototype.handleKeyDown = function($item, e) {
 				var clusterMeta = cluster.find('h2').first();
 
 				// Add class request
-        var saved = createClass(newClassName, clusterMeta.data('coord-x'), clusterMeta.data('coord-y'));
+				var saved = createClass(newClassName, clusterMeta.data('coord-x'), clusterMeta.data('coord-y'));
 
-        if (saved) {
-          changeStatus('Successfully created new class "' + newClassName + '".');
-          isAddMode = false;
-        } else {
-          changeStatus('Creating new class failed. Please try again.');
-        }
+				if (saved) {
+					changeStatus('Successfully created new class "' + newClassName + '".');
+					isAddMode = false;
+				} else {
+					changeStatus('Creating new class failed. Please try again.');
+				}
 
 			} else if ($item.is('.tree-parent')) {
 				// toggle the child group open or closed
