058f0d1c136dbdb21481e3932ffc58e0c6a54df0
Sebastian Hahner <hahner@fzi.de>
2017-06-26 15:32:36 +0200

    Fixed the swagger issue. swagger.json gets now generated properly.

--- bundles/de.cooperateproject.cdo.dawn.rest/src/de/cooperateproject/cdo/dawn/rest/Activator.java
+++ bundles/de.cooperateproject.cdo.dawn.rest/src/de/cooperateproject/cdo/dawn/rest/Activator.java
@@ -15,6 +15,10 @@ import de.cooperateproject.cdo.dawn.rest.util.CORSFilter;
 import de.cooperateproject.cdo.dawn.rest.util.EMFReadyProvider;
 import de.cooperateproject.cdo.dawn.rest.util.ServiceFactory;
 import de.cooperateproject.cdo.dawn.rest.util.ServiceRegistry;
+import io.swagger.config.ScannerFactory;
+import io.swagger.jaxrs.config.SwaggerContextService;
+import io.swagger.jaxrs.config.SwaggerScannerLocator;
+
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 
@@ -26,8 +30,7 @@ public class Activator implements BundleActivator {
 
 		registerProviders(bundleContext);
 		
-		// FIXME: Configuration admin not found, although equinox is activated
-		//configureSwagger(bundleContext);
+		configureSwagger(bundleContext);
 
 		serviceRegistry.addService(bundleContext.registerService(BrowseService.class,
 				ServiceFactory.getInstance().getBrowseService(), null));
@@ -55,9 +58,8 @@ public class Activator implements BundleActivator {
 		bundleContext.registerService(CORSFilter.class, corsFilter, null);
 	}
 
-	@SuppressWarnings("unused")
 	private void configureSwagger(BundleContext bundleContext) throws Exception {
-		ServiceReference<?> reference = bundleContext.getServiceReference(ConfigurationAdmin.class);
+		ServiceReference<?> reference = bundleContext.getServiceReference(ConfigurationAdmin.class.getName());
 		ConfigurationAdmin configAdmin = (ConfigurationAdmin) bundleContext.getService(reference);
 		Configuration configuration = configAdmin.getConfiguration("com.eclipsesource.jaxrs.swagger.config", null);
 
@@ -65,11 +67,15 @@ public class Activator implements BundleActivator {
 
 		properties.put("swagger.basePath", "/services");
 		properties.put("swagger.host", "localhost:9090");
-		properties.put("swagger.info.title", "Dawn Web Editor API");
+		properties.put("swagger.info.title", "Dawn WEB Editor API");
+		properties.put("swagger.info.version", "1.0.0");
 		properties.put("swagger.info.license.name", "Eclipse Public License, version 1.0");
 		properties.put("swagger.info.license.url", "http://www.eclipse.org/legal/epl-v10.html");
 		configuration.update(properties);
 		bundleContext.ungetService(reference);
+		
+		// Add OSGIJaxRsScanner to the swagger scanner locator to get loaded properly
+		SwaggerScannerLocator.getInstance().putScanner(SwaggerContextService.SCANNER_ID_DEFAULT, ScannerFactory.getScanner());
 	}
 
 }
