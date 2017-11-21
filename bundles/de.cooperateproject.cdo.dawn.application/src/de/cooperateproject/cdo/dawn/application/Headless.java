bfa1dec680966d8eff75fc6ef92c56ccc5a98fc4
Stephan Seifermann <seifermann@fzi.de>
2017-11-21 17:31:29 +0100

    Made product build ready for dropins

--- bundles/de.cooperateproject.cdo.dawn.application/src/de/cooperateproject/cdo/dawn/application/Headless.java
+++ bundles/de.cooperateproject.cdo.dawn.application/src/de/cooperateproject/cdo/dawn/application/Headless.java
@@ -1,18 +1,37 @@
 package de.cooperateproject.cdo.dawn.application;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Headless implements IApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Headless.class);
+	private static final String APP_NAME = "Cooperate CDO Dawn Backend";
+	private CompletableFuture<Object> stopFuture;
+
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
-		return IApplication.EXIT_OK;
+		LOGGER.info("{} starting...", APP_NAME);
+
+		context.applicationRunning();
+
+		stopFuture = new CompletableFuture<Object>();
+		Object result = stopFuture.get();
+		stopFuture = null;
+
+		LOGGER.info("{} terminating...", APP_NAME);
+		return result;
 	}
 
 	@Override
 	public void stop() {
-		return;
+		if (stopFuture != null) {
+			stopFuture.complete(EXIT_OK);
+		}
 	}
 
-}	
+}
